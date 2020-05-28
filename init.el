844633fa3375dea11d3af79f6fe33a6570ed9ad7
inmove <lisper.inmove@gmail.com>
2020-05-29 01:01:10 +0800

    add .gitignore

--- init.el
+++ init.el
@@ -19,3 +19,9 @@
    (quote
     (exec-path-from-shell spaceline-all-the-icons spaceline vue-mode elscreen treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs htmlize magit elpy use-package monokai-theme))))
 (put 'narrow-to-region 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
