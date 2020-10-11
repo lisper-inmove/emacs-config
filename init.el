63356948e79650d0bd38035a18a65d5b40b9edd0
inmove <lisper.inmove@gmail.com>
2020-10-11 23:59:42 +0800

    done

--- init.el
+++ init.el
@@ -7,9 +7,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(git-gutter:added-sign "☀")
+ '(git-gutter:ask-p nil)
+ '(git-gutter:deleted-sign "☂")
+ '(git-gutter:disabled-modes (quote (asm-mode image-mode)))
+ '(git-gutter:lighter " GG")
+ '(git-gutter:modified-sign "☁")
+ '(git-gutter:separator-sign "|")
+ '(git-gutter:update-interval 1)
+ '(git-gutter:window-width 2)
  '(package-selected-packages
    (quote
-    (fuzzy-finder wgrep helm-addressbook helm-ls-git yasnippet-snippets yasnippet noccur color-moccur elscreen iedit htmlize ace-window auto-package-update magit ace-jump-mode helm monokai-theme undo-tree use-package))))
+    (treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs elpy dockerfile-mode git-gutter fuzzy-finder wgrep helm-addressbook helm-ls-git yasnippet-snippets yasnippet noccur color-moccur elscreen iedit htmlize ace-window auto-package-update magit ace-jump-mode helm monokai-theme undo-tree use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
