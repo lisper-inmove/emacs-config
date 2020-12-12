e091159c6c5aa10d178911c18f990170680c8c3d
inmove <lisper.inmove@gmail.com>
2020-12-12 23:31:43 +0800

    init

--- init.el
+++ init.el
@@ -1,17 +1,3 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/basic.org"))
-(org-babel-load-file (expand-file-name "~/.emacs.d/my-functions.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (fuzzy-finder wgrep helm-addressbook helm-ls-git yasnippet-snippets yasnippet noccur color-moccur elscreen iedit htmlize ace-window auto-package-update magit ace-jump-mode helm monokai-theme undo-tree use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file (expand-file-name "~/.emacs.d/functions.org"))
