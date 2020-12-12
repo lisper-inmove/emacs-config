06329d20d75892b9a414d954a974c9099d31671b
inmove <lisper.inmove@gmail.com>
2020-12-13 00:12:10 +0800

    done

--- init.el
+++
@@ -1,3 +0,0 @@
-(org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
-(org-babel-load-file (expand-file-name "~/.emacs.d/basic.org"))
-(org-babel-load-file (expand-file-name "~/.emacs.d/functions.org"))
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Tangle files with Org mode
+
+echo "install flake8"
+pip install flake8
+cp init.src.el init.el
