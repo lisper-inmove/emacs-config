6acc20e34f961b43f43517223e23e9acf39e5241
inmove <lisper.inmove@gmail.com>
2020-12-14 19:32:34 +0800

    position register

--- init.src.el
+++ init.src.el
@@ -1,5 +1,5 @@
-(org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/basic.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/functions.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/publish.org"))
 (custom-set-variables
