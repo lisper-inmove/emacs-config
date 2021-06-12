ef59b518dcd0ecf536695b0db94fffb4b9c70041
inmove <lisper.inmove@gmail.com>
2021-06-12 22:28:16 +0800

    emacs config for python

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
+(org-babel-load-file (expand-file-name "~/.emacs.d/basic.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/functions.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/publish.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,5 +20,11 @@
  '(org-agenda-files (quote ("~/code/blog/org-mode.org" "~/code/blog/TODO.org")))
  '(package-selected-packages
    (quote
-    (exec-path-from-shell spaceline-all-the-icons spaceline vue-mode elscreen treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs htmlize magit elpy use-package monokai-theme))))
+    (flycheck-clangcheck flycheck-pyflakes company-irony-c-headers company-irony helm-ls-git helm yasnippet-snippets protobuf-mode docker ini-mode org-bullets json-mode counsel find-file-in-project git-gutter iedit ace-window undo-tree py-autopep8 company-jedi exec-path-from-shell spaceline-all-the-icons spaceline vue-mode elscreen treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs htmlize magit elpy use-package monokai-theme))))
 (put 'narrow-to-region 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
