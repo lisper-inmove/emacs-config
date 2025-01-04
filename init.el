0d55700fc87f4a708d695da7bef617a9f3b3fec3
inmove <lisper.inmove@gmail.com>
2025-01-04 15:16:43 +0800

    done

--- init.el
+++ init.el
@@ -36,3 +36,23 @@
 (load-org "~/.emacs.d/plugins")
 (load-org "~/.emacs.d/program-language")
 (load-org "~/.emacs.d/postload")
+
+(add-to-list 'load-path "~/.emacs.d/lsp-bridge/")
+(require 'yasnippet)
+(yas-global-mode 1)
+(require 'lsp-bridge)
+(global-lsp-bridge-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(web-mode typescript-mode lsp-tailwindcss protobuf-mode flutter lsp-dart dockerfile-mode yasnippet-snippets yasnippet which-key w3m try rainbow-mode rainbow-delimiters magit k8s-mode json-mode helm-gtags helm-ls-git helm flycheck-pycheckers flycheck-mypy flycheck-pyflakes find-file-in-project elscreen elisp-demos docker counsel company-irony-c-headers company-irony company bm all-the-icons ace-window ace-jump-mode yaml-mode wfnames projectile powerline org-bullets neotree monokai-theme lua-mode lsp-mode json-snatcher iedit idle-highlight-mode htmlize goto-last-change git-gutter flycheck emmet-mode compat)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
