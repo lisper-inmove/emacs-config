518d4116abe41d3ad052baa33f7dd2a4c4cdb1e5
inmove <lisper.inmove@gmail.com>
2025-01-04 12:35:51 +0800

    done

--- init.el
+++ init.el
@@ -2,12 +2,12 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 ; 使用清华源源: https://mirrors.tuna.tsinghua.edu.cn/help/elpa/
-;; (setq package-archives '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-;;                          ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
-;;                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/stable-melpa/")
-;;                          ))
-(setq package-archives '(("gnu"   . "http://1.15.88.122/gnu/")
-                         ("melpa" . "http://1.15.88.122/melpa/")))
+(setq package-archives '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/stable-melpa/")
+                         ))
+;; (setq package-archives '(("gnu"   . "http://1.15.88.122/gnu/")
+;;                          ("melpa" . "http://1.15.88.122/melpa/")))
 
 ;; (setq package-archives
 ;;       '(("gnu" . "https://elpa.gnu.org/packages/")
@@ -36,27 +36,3 @@
 (load-org "~/.emacs.d/plugins")
 (load-org "~/.emacs.d/program-language")
 (load-org "~/.emacs.d/postload")
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(git-gutter:added-sign "☀")
- '(git-gutter:ask-p nil)
- '(git-gutter:deleted-sign "☂")
- '(git-gutter:disabled-modes '(asm-mode image-mode))
- '(git-gutter:lighter " GG")
- '(git-gutter:modified-sign "☁")
- '(git-gutter:separator-sign "|")
- '(git-gutter:update-interval 1)
- '(git-gutter:window-width 2)
- '(package-selected-packages
-   '(flycheck-pycheckers flycheck-mypy lsp-tailwindcss tree-sitter-langs centered-cursor-mode toml-mode w3m rainbow-mode eglot emmet-mode scss-mode company-web dockerfile-mode lsp-java impatient-mode company-solidity solidity-flycheck solidity-mode cargo-mode cargo rust-mode ini-mode powerline flutter lsp-dart lua-mode org-bullets neotree neo-tree all-the-icons pyim-wudict pyim-wbdict pyim elisp-demos bm goto-last-change json-mode yaml-mode smartparens browse-kill-ring idle-highlight-mode rainbow-delimiters try protobuf-mode flycheck treemacs-magit flycheck-pyflakes flymake-python-pyflakes yasnippet-snippets yasnippet undo-tree treemacs-persp treemacs-icons-dired treemacs-projectile treemacs-evil treemacs htmlize helm-gtags helm-ls-git helm find-file-in-project elscreen counsel ace-window ace-jump-mode use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
