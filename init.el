869716e65c52f8cdd9ec886968a6dc5811dd51da
谢昭辔 <xie.zhaopei@pmish-tech.com>
2025-09-17 11:46:46 +0800

    1

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
   (dolist (file config-files) (org-babel-load-file file))
   )
 
-(load-org "~/.emacs.d/themes")
+;; (load-org "~/.emacs.d/themes")
 (load-org "~/.emacs.d/preload")
 (load-org "~/.emacs.d/plugins")
 (load-org "~/.emacs.d/program-language")
